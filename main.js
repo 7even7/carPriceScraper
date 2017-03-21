8f20a99a5c56f65af38bb74eb6ab76f7b4746c1d
Unknown <patrick.stahl@gmail.com>
2017-03-21 13:12:45 +0200

    Car objects created from list page

--- main.js
+++ main.js
@@ -17,10 +17,61 @@ page.then((content)=>{
 
 function createCarObjectsFromResultPage (body){
     var $ = cheerio.load(body);
+    var lkm = 0
     $('div.data_box').each(function(i, listItem){
           // Scrape URL and price from car list.
           var listItemRoot = listItem.parent.parent;
           var URL=$('.childVifUrl.tricky_link', listItemRoot).attr('href');
-          console.log(URL);
+          var scrapedCar = new Car(URL);
+          scrapedCar._id=URL.split('/')[URL.split('/').length-1];
+          // update car-object with rest of the data
+          var price = $('.main_price',this).text().replace(/\D/g,'')
+          scrapedCar.price = price; 
+          scrapedCar.engine=$('.eng_size', this).text().replace(/[^0-9\.]+/g,""); 
+          // Make and model need to be parsed from the same string
+          var makeModel = $('.make_model_link', this).text().split(' ');
+          scrapedCar.make=makeModel[0];
+          scrapedCar.model=makeModel[1];
+          // Seller info
+          scrapedCar.location=$('.list_seller_info',this).children('b').text();
+          scrapedCar.seller=$('.list_seller_info',this).children('span').text()
+          
+          // Details parsed from List items. Sometimes one of the info pieces is missing and therefore this works only if all 4 are present.
+          var otherInfo = $(this).find('li')
+          if (otherInfo.length==4){
+            scrapedCar.buildYear=otherInfo[0].children[0].data;
+            scrapedCar.milage=otherInfo[1].children[0].data.replace(/\D/g,'');
+            scrapedCar.fuelType=otherInfo[2].children[0].data;
+            scrapedCar.transmission=otherInfo[3].children[0].data;
+          console.log("auto nro: " + lkm)
+          console.log(scrapedCar);
+          lkm++;
+          }
     })
+    
+    
+}
+
+
+
+// Declarations
+function Car(URL){
+  this.URL=URL;
+  this._id=null;
+  this.siteID = "Nettiauto";
+  this.make=null;
+  this.model=null;
+  this.buildYear=null;
+  this.plateNumber=null;
+  this.drive=null;
+  this.transmission=null;
+  this.engine=null;
+  this.fuelType=null;
+  this.milage=null;
+  this.firstDate;
+  this.lastDate=null;
+  this.price=null;
+  this.priceHistory = [];
+  this.location="";
+  this.seller="";
 }
